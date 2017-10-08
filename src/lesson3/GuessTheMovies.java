e5788a9cae3d89ffd56da4acc588ef57ec2f706c
NazarTsybulskyi <naz.tsybulskyi@gmail.com>
2017-10-08 22:45:41 +0300

    guess the movie - 10/8/2017

--- src/lesson3/GuessTheMovies.java
+++ src/lesson3/GuessTheMovies.java
@@ -1,8 +1,25 @@
 package lesson3;
+import java.util.Arrays;
 import java.util.Scanner;
+import java.io.File;
+import java.math.*;
+
+import static java.lang.Math.*;
 
 public class GuessTheMovies {
-    public static void main (String[] args) {
-
+    public static void main (String[] args) throws Exception{
+        File f = new File("MoviesList.txt");
+        Scanner sc = new Scanner(f);
+        String line = sc.nextLine();
+        String[] movTitles = line.split(",");
+        //System.out.println(Arrays.toString(movTitles));
+        int movNumber = (int) (Math.random()*10);
+        //System.out.println(movNumber);
+        String movieName = movTitles[movNumber];
+        String movieName_hidden = "";
+        for (int i = 0; i < movieName.length(); i++) {
+            movieName_hidden += "_ ";
+        }
+        System.out.println(movieName_hidden);
     }
 }
