77d896771d63d6291656e28b2affb3b3441246c0
Nazar Tsybulskyi <naz.tsybulskyi@gmail.com>
2017-10-11 17:16:42 +0300

    9/26/17 - lesson2

--- src/lesson3/GuessTheMovies.java
+++ src/lesson3/GuessTheMovies.java
@@ -15,11 +15,17 @@ public class GuessTheMovies {
         //System.out.println(Arrays.toString(movTitles));
         int movNumber = (int) (Math.random()*10);
         //System.out.println(movNumber);
-        String movieName = movTitles[movNumber];
-        String movieName_hidden = "";
-        for (int i = 0; i < movieName.length(); i++) {
-            movieName_hidden += "_ ";
+        String movieName_hidden = MovieService.getUndersoredMovieName(movTitles[movNumber]);
+
+        boolean hasWon = false;
+        int pointCounter = 0;
+        Scanner scanner = new Scanner(System.in);
+        while (!hasWon) {
+            System.out.println(movieName_hidden);
+            System.out.println("Name one letter. You have " + pointCounter + " points");
+            char guess = scanner.next().charAt(0);
+
         }
-        System.out.println(movieName_hidden);
+
     }
 }
--- src/lesson3/MovieService.java
+++ src/lesson3/MovieService.java
@@ -1,16 +1,31 @@
 package lesson3;
 
+import java.util.ArrayList;
+
+import static java.lang.Character.isLetter;
+
 public class MovieService {
-    String name;
+    String movieName;
 
-    public Integer getCharPosition(Character c) {
-
-    return 0;
-
+    public static String getUndersoredMovieName(String movieName) {
+        String nameWithoutSpaces = movieName.replaceAll("\\s","");
+        String underscName = "";
+        for (int i = 0; i < nameWithoutSpaces.length()-1; i++) {
+            if (isLetter(nameWithoutSpaces.charAt(i)))
+                underscName += "_ ";
+        }
+        return  underscName;
     }
 
-    public Integer getNameLength() {
-        return 0;
-
+    public static Integer[] getCharIndices(Character c, String movieName) {
+        ArrayList<Integer> indices = new ArrayList<Integer>();
+        int count = 0;
+        for (int i = 0; i < movieName.length()-1; i++) {
+            if (movieName.charAt(i) == c) {
+                indices.add(i);
+                count++;
+            }
+        }
+        return (Integer[]) indices.toArray();
     }
 }
