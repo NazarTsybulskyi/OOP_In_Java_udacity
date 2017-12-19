6883ec4000c9ca81d161ce3ed9b0a59e7acedee0
NazarTsybulskyi <naz.tsybulskyi@gmail.com>
2017-12-19 10:55:52 +0200

    updated list of movies

--- src/lesson3/GuessTheMovies.java
+++ src/lesson3/GuessTheMovies.java
@@ -4,27 +4,6 @@ import java.io.File;
 
 public class GuessTheMovies {
     public static void main (String[] args) throws Exception{
-        File f = new File("MoviesList.txt");
-        Scanner sc = new Scanner(f);
-        String line = sc.nextLine();
-        String[] movTitles = line.split(",");
-        //System.out.println(Arrays.toString(movTitles));
-        int movNumber = (int) (Math.random()*10);
-        //System.out.println(movNumber);
-        String movieName_hidden = MovieService.getUndersoredMovieName(movTitles[movNumber]);
-
-        boolean hasWon = false;
-        int pointCounter = 0;
-        Scanner scanner = new Scanner(System.in);
-        while (!hasWon || pointCounter < 10) {
-            System.out.println(movieName_hidden);
-            System.out.println("Name one letter. You have " + pointCounter + " points");
-            char guess = scanner.next().charAt(0);
-            if (MovieService.getCharIndices(guess, movTitles[movNumber]).length > 0) {
-                System.out.println("There is letter " + guess + " in this movie title!");
-                System.out.println();
-            }
-        }
 
     }
 }
--- src/lesson3/MovieService.java
+++ src/lesson3/MovieService.java
@@ -5,37 +5,5 @@ import java.util.ArrayList;
 import static java.lang.Character.isLetter;
 
 public class MovieService {
-    String movieName;
 
-    public static void greeting() {
-        System.out.println("Welcome to Guess the Movie!");
-        System.out.println("The rules are simple, the computer randomly picks a movie title, and shows you how " +
-                "many " + "letters it's made up of.");
-        System.out.println("Your goal is to try to figure out the movie by guessing one letter at a time.");
-        System.out.println("If a letter is indeed in the title the computer will reveal its correct position in" +
-                " the " + "word, if not, you lose a point.");
-        System.out.println("If you lose 10 points, game over!");
-        System.out.println("Let's start!");
-    }
-
-    public static String getUndersoredMovieName(String movieName) {
-        String underscoredName = "";
-        for (int i = 0; i < movieName.length()-1; i++) {
-            if (isLetter(movieName.charAt(i)))
-                underscoredName += "_ ";
-        }
-        return  "You are guessing: " + underscoredName;
-    }
-
-    public static Integer[] getCharIndices(Character c, String movieName) {
-        ArrayList<Integer> indices = new ArrayList<Integer>();
-        int count = 0;
-        for (int i = 0; i < movieName.length()-1; i++) {
-            if (movieName.charAt(i) == c) {
-                indices.add(i);
-                count++;
-            }
-        }
-        return (Integer[]) indices.toArray();
-    }
 }
